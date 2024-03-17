38f089a7587946b4df669f2565a0d3d9525a972a
Kieron Rees <140263951+chimpankie@users.noreply.github.com>
2024-03-17 14:52:38 +0000

    Add working loop function to create library

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
-
+let shelf = document.querySelector('#bookGrid');
 
 const myLibrary = [];
 
-function Book (title, author, year, rating){
+function Book (title, author, year, rating, read){
     this.title = title;
     this.author = author;
     this.year = year;
     this.rating = rating;
+    this.read = read; 
 }
 
-function addBookToLibrary(title, author, year, rating){
-    let book = new Book(title, author, year, rating);
+function addBookToLibrary(title, author, year, rating, read){
+    let book = new Book(title, author, year, rating, read);
     myLibrary.push(book);
 
 }
@@ -19,5 +20,43 @@ function addBookToLibrary(title, author, year, rating){
 function createLibrary(){
     for(let i = 0; i<myLibrary.length; i++){
     console.log(myLibrary[i]);
+
+    //assign object to variable within loop
+
+    let objectBook = myLibrary[i];
+
+    //create book card and append to book shelf
+    let book = document.createElement("div");
+    shelf.appendChild(book);
+
+    // Create DIV elements to sit on book card 
+
+    let title = document.createElement("h2");
+    let author = document.createElement("h3");
+    let year = document.createElement("p");
+    let rating = document.createElement("p");
+    let read = document.createElement('p');
+
+    //assign object proprties to DOM elements
+    console.log(objectBook.title);
+    
+    title.textContent = objectBook.title; 
+    author.textContent = objectBook.author;
+    year.textContent = objectBook.year; 
+    rating.textContent = objectBook.rating;
+    read.textContent = objectBook.read; 
+   
+    //append new DOM elements to book card
+
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(year);
+    book.appendChild(rating);
+    book.appendChild(read); 
+    
+
+
+
+    
     }
 };
